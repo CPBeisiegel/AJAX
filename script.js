d2b11fe6ae34e65790fbffe5484f85ff3390ba06
CPBeisiegel <camila.beisiegel@gmail.com>
2019-07-24 12:24:41 -0300

    ajax

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let titulo = document.querySelector('.titulo')
 let clienteId = document.querySelector('.clienteId')
 let button = document.querySelector('#puxar')
+let buttonImg = document.querySelector('#puxarImg')
 
 button.onclick = function (){
 
@@ -16,7 +17,26 @@ button.onclick = function (){
     titulo.textContent = json.title
     clienteId.textContent = json.userId
     })
-    
+
+}
+
+buttonImg.onclick = function (){
+  fetch('https://jsonplaceholder.typicode.com/photos')
+  .then(response => response.json())
+//   .then(json => console.log(json))
+  .then(function (json){
+    //console.log(json)
+    let div = document.querySelector('div')
+   for(let i=0;i< 10; i++){
+    // informações da api 
+    let img = json[i]
+      // uma tag recem criada 
+     let criarImg = document.createElement('img');
+     criarImg.setAttribute('src', img.url)
+    // para cada img que eu receber, vou pegar ela e colocar dentro da div.
+     div.appendChild(criarImg)
+   }
+    })
 }
 
 
